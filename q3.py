ab57f8bbcc904e348c505737621bd8b12a65316f
plus0510 <cy0510@naver.com>
2022-11-01 17:51:38 +0900

    20221101end

---
+++ q3.py
@@ -0,0 +1,74 @@
+#조건문
+
+#내가 43250원이 있다.
+#만원짜리는 몇개있고
+#오천원짜리는 몇개있고
+#500원은 몇개있고
+#100원은 몇개있고
+#50원은 몇개있고 
+#10원은 몇개있다.
+
+
+# money=43250
+# print(f"내가 {money}원이 있다.")
+# if money // 10000 >=1:
+#     m10000=money//10000
+#     print( f"만원짜리는 {m10000}개 있고")
+#     money -= (m10000*10000)
+# else:
+#     print( f"만원짜리는 0개 있고")
+# if money // 5000 >=1:
+#     m5000=money//5000
+#     print( f"오천원짜리는 {m5000}개 있고") 
+#     money -= m5000*5000
+# else:
+#     print( f"오천원짜리는 0개 있고")
+# if money // 500 >=1:
+#     m500=money//500
+#     print( f"오백원짜리는 {m500}개 있고")
+#     money -= m500*500
+# else:
+#     print( f"오백원짜리는 0개 있고")
+# if money // 100 >=1:
+#     m100=money//100
+#     print( f"백원짜리는 {m100}개 있고")
+#     money -= m100*100
+# else:
+#     print( f"백원짜리는 0개 있고")
+# if money // 50 >=1:
+#     m50=money//50
+#     print( f"오십원짜리는 {m50}개 있고")
+#     money -= m50*50
+# else:
+#     print( f"오십원짜리는 0개 있고.")
+# if money // 10 >=1:
+#     m10=money//10
+#     print( f"십원짜리는 {m10}개 있다.")
+#     money -= m10*10        
+# else:
+#     print( f"십원짜리는 0개 있다.")
+
+
+# 12
+#a = 12
+#print(f"{a}는 2와 3의 공배수입니다")
+#a = 11
+#print(f"{a}는 2와 3의 공배수아닙니다")
+#a = 10
+#print(f"{a}는 2의 배수입니다")
+#a = 9
+#print(f"{a}는 3의 배수입니다")
+
+# a=45
+# answer = f"{a}는 "
+# if a%2 ==0 and a%3 == 0 :
+#     answer += "2와 3의 공배수입니다"
+# elif a%2 == 0 or a%3 == 0 :
+#     if a%2 == 0 :
+#         answer += "2의 배수입니다"
+#     elif a%3 == 0 :   
+#         answer += "3의 배수입니다"
+# else :
+#     answer += "2와 3의 공배수아닙니다"
+# print(answer)
+
---
+++ q4.py
@@ -0,0 +1,141 @@
+#반복문
+
+# a= [1,2,3]
+# print(a[0])
+# print(a[1])
+# print(a[2])
+
+a= [1,2,3]
+# 디버그 
+
+# for 변수 in range(범위) :
+# range(start, stop[, step]) step 2씩증가 같은거 -2씩으로 역순도 가능
+# range(0, len(a)) = 0, 1, 2 0<=i<3
+# =list
+
+# for i in range(0, len(a)):
+#     print(i)
+#     print(a[i])
+
+a=[5,8,7,9,10]
+
+# for i in range(0,len(a) ) :
+#     answer = f"{a[i]}는 "
+#     if a[i]%2 ==0 and a[i]%3 == 0 :
+#         answer += "2와 3의 공배수입니다"
+#     elif a[i]%2 == 0 or a[i]%3 == 0 :
+#         if a[i]%2 == 0 :
+#             answer += "2의 배수입니다"
+#         elif a[i]%3 == 0 :   
+#             answer += "3의 배수입니다"
+#     else :
+#         answer += "2와 3의 공배수아닙니다"
+#     print(answer)
+
+#for i in range(0,len(a) ) :
+# for el in a :
+#     if el %2==0:
+#         print(f"{el} > 짝수")
+#     else :
+#         print(f"{el} > 홀수")
+
+# #별찍기
+
+# s="*"
+
+# for i in range(5, 0,-1):
+#     print(f"{s}"*i)
+        
+# 딕셔너리화
+
+# person1 = {"name":"kim", "age":13}
+# person2 = {"name":"lee", "age":15}
+# person3 = {"name":"park", "age":17}
+# a=[person1, person2, person3]
+# person_excel = [["name","age"],["kim",13],["park",17],["lee",15]]
+# keys=person_excel[0] # ["name","age"]
+# datas=person_excel[1:] # ["kim",13],["park",17],["lee",15]
+# saves=[]
+# for data in datas: #[0,1,2]
+#     tmp={}
+#     for i in range(0,len(keys)): #[0, 1] i=0
+#         tmp[keys[i]]=data[i]
+#         # i=0, keys[0] = "name", data[0] = "kim" {'name': 'kim'}
+#         # i=1, keys[1] = "age", data[1] = "13" {'name': 'kim','age' : 13}
+#     saves.append(tmp)
+# print(saves)
+#     #     tmp[key] = ""
+#     # for  el in data:
+#     #     tmp[key] = el
+#     # range(0,len(keys))=[0,1]
+
+# money=43250
+# print(f"내가 {money}원이 있다.")
+# m = [10000, 5000, 1000, 500, 100, 50, 10]
+# for won in m:
+#     if money // won >=1:
+#         print( f"{won}짜리는 {money//won}개 있다.")
+#         money -= (money//won)*won
+#     else:
+#         print( f"{won}짜리는 0개 있다.")
+
+
+# b=[47, 90, 1, 22, 9, 5]
+# for a in b:
+#     answer = f"{a}는 "
+#     if a%2 ==0 and a%3 == 0 :
+#         answer += "2와 3의 공배수입니다"
+#     elif a%2 == 0 or a%3 == 0 :
+#         if a%2 == 0 :
+#          answer += "2의 배수입니다"
+#         elif a%3 == 0 :   
+#             answer += "3의 배수입니다"
+#     else :
+#         answer += "2와 3의 공배수아닙니다"
+#     print(answer)
+
+# a=[47, 90, 1, 22, 9, 5]
+# cnt1, cnt2 = 0, 0
+# for el in a:
+#     answer = f"{el}는 "
+#     if el%2 == 0 :
+#         answer += "짝수입니다."
+#         cnt2 += 1
+#     else :
+#         answer += "홀수입니다."
+#         cnt1 += 1
+#     print(answer)
+# print(f"홀수는 {cnt1}개 이고 짝수는 {cnt2}개입니다.")
+
+a=[47, 90, 1, 22, 9, 5]
+tmp = {
+        0:{"str":"2와 3의 공배수도 아닌 것은 ","count":0}, 
+        2:{"str":"2의 배수만인 것은 ","count":0}, 
+        3:{"str":"3의 배수만인 것은 ","count":0}, 
+        6:{"str":"2와 3의 공배수는 ","count":0}
+        }
+
+for el in a:
+    answer = f"{el}는 "
+    if el%2 ==0 and el%3 == 0 :
+        answer += "2와 3의 공배수입니다"
+        tmp[6]['count'] = tmp.get(0).get('count')+1
+    elif el%2 == 0 or el%3 == 0 :
+        if el%2 == 0 :
+            answer += "2의 배수입니다"
+            tmp[2]['count'] = tmp.get(0).get('count')+1
+        elif el%3 == 0 :   
+            answer += "3의 배수입니다"
+            tmp[3]['count'] = tmp.get(0).get('count')+1
+    else :
+        answer += "2와 3의 공배수아닙니다"
+        tmp[0]['count'] = tmp.get(0).get('count')+1
+    print(answer)
+
+# print(f"2와 3의 공배수는 개입니다.")
+# print(f"2의 배수만인 것은 개입니다.")
+# print(f"3의 배수만인 것은 개입니다.")
+# print(f"2와 3의 공배수도 아닌 것은 개입니다.")
+for key in list(tmp.keys()):
+    print(f"{tmp.get(key).get('str')}{tmp.get(key).get('count')} 개입니다.")
+
