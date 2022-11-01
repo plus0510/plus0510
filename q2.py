dea9e2cdee4c6c771bd0135104db6fbc2e6acccf
plus0510 <cy0510@naver.com>
2022-11-01 11:44:36 +0900

    20221101-1144am

--- q2.py
+++ q2.py
@@ -5,3 +5,18 @@ from operator import getitem
 
 #두번째
 #세번째
+#조건문
+#5는 짝수 True False
+#5는 짝수입니다 홀수입니다 출력
+num=5
+if num % 3 == 0:                    #첫번째조건
+    print(f"{num}는 3의배수입니다.")
+elif num % 3 == 1:                  #두번째조건
+    print(f"{num}는 3의배수가 아닙니다.")
+elif num % 3 == 2:                  #세번째조건
+    print(f"{num}는 3의배수가 아닙니다.")
+else:                               #다 아닐때
+    print("else")
+    print("다")
+    print("아님")
+
